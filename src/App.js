4b8fbee18a2325bf6c03d337ce04290efaea5fbc
FrederickKisife <kilafrederick@gmaul.com>
2023-10-27 07:11:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import profile1 from './Images/1.jpg';
+import profile2 from './Images/2.jpg';
+import profile3 from './Images/3.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="ui comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img src={profile1} alt="profile picture" width={150} height={150}/>
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Frederick
+            </a>
+            <div className="metadata">
+              <span>
+                Today at : 5:00am
+              </span>
+            </div>
+            <div className="text">
+              It's Amazing!!!!
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ui comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img src={profile2} alt="profile picture" width={150} height={150}/>
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Hansel
+            </a>
+            <div className="metadata">
+              <span>
+                Today at : 1:00pm
+              </span>
+            </div>
+            <div className="text">
+              I love what I see it's beautiful!!!
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="ui comments">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img src={profile3} alt="profile picture" width={150} height={150}/>
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              Frederick
+            </a>
+            <div className="metadata">
+              <span>
+                Today at : 10:00pm
+              </span>
+            </div>
+            <div className="text">
+              It's just super waooow!!!!!
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
